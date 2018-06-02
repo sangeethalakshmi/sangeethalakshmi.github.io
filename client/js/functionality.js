90f23114633f64724c438d858c42b959e89b88d7
Sangeetha <sangeetha@jayeesha.com>
2018-06-02 12:05:44 +0530

    activities new html

--- client/js/functionality.js
+++ client/js/functionality.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var site_url = "http://renukakatta.org/sangeethalakshmi.github.io/server/index.php/"
+var site_url = "http://renukakatta.org/v2/server/index.php/"
 if(localStorage.getItem('usertype') == 'Super Admin'){
   $('.notforadmin').hide();
   $('.foradmin').show();
