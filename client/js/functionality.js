cc6b266a25e4d62ef5b0fd4fce5e04753aa095d6
sangeethalakshmi <sangeethalakshmi239@gmail.com>
2018-11-22 12:02:01 +0530

    Update functionality.js

--- client/js/functionality.js
+++ client/js/functionality.js
@@ -87,7 +87,7 @@ function doLogout (){
 window.fbAsyncInit = function() {
     // FB JavaScript SDK configuration and setup
     FB.init({
-      appId      : '378427832540873', // FB App ID
+      appId      : 'XXXXXXXXXXXXX', // FB App ID
       cookie     : true,  // enable cookies to allow the server to access the session
       xfbml      : true,  // parse social plugins on this page
       version    : 'v2.8' // use graph api version 2.8
